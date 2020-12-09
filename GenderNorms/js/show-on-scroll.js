4dae66c1c8f11705591d4f749d77a929c06cf4a2
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2020-12-08 23:17:20 -0500

    zzzzzz

--- GenderNorms/js/show-on-scroll.js
+++ GenderNorms/js/show-on-scroll.js
@@ -26,13 +26,12 @@ loop();
 
 // Helper function from: http://stackoverflow.com/a/7557433/274826
 function isElementInViewport(el) {
-  // special bonus for those using jQuery
   if (typeof jQuery === "function" && el instanceof jQuery) {
     el = el[0];
   }
   var rect = el.getBoundingClientRect();
   return (
-    (rect.top <= 150 
+    (rect.top <= 150
       && rect.bottom >= 0)
     ||
     (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
